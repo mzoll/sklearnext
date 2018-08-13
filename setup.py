ae094db02498e3f9ac510728764988fc67b9412a
marcel zoll <marcel.zoll@nowinteract.com>
2018-08-13 11:13:07 +0200

    raise version to 0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sklearnext',
-      version='1.3',
+      version='0.1.3',
       description='pandas compatibility and pipelining extensions to scikit-learn',
       url='http://github.com/mzoll/sklearnext',
       author='Marcel Zoll',
